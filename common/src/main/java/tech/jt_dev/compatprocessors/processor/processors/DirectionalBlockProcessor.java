150df2cb9f357c99346c4db1eb3b9beb9362b583
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-01 15:52:01 -0400

    finish DirectionalBlockProcessor.java

--- common/src/main/java/tech/jt_dev/compatprocessors/processor/processors/DirectionalBlockProcessor.java
+++ common/src/main/java/tech/jt_dev/compatprocessors/processor/processors/DirectionalBlockProcessor.java
@@ -1,4 +1,4 @@
-package tech.jt_dev.compatprocessors.processor;
+package tech.jt_dev.compatprocessors.processor.processors;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -22,14 +22,17 @@ import org.jetbrains.annotations.Nullable;
 public class DirectionalBlockProcessor extends StructureProcessor {
 
     public static final Codec<DirectionalBlockProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            BuiltInRegistries.BLOCK.byNameCodec().fieldOf("old").forGetter((old) -> old.old),
             BuiltInRegistries.BLOCK.byNameCodec().fieldOf("block").forGetter((block) -> block.block),
             Codec.FLOAT.fieldOf("chance").forGetter((chance) -> chance.chance)
     ).apply(instance, DirectionalBlockProcessor::new));
 
+    private final Block old;
     private final Block block;
     private final float chance;
 
-    public DirectionalBlockProcessor(Block block, float chance) {
+    public DirectionalBlockProcessor(Block old, Block block, float chance) {
+        this.old = old;
         this.block = block;
         this.chance = chance;
     }
@@ -37,9 +40,11 @@ public class DirectionalBlockProcessor extends StructureProcessor {
 
     @Override
     public @Nullable StructureTemplate.StructureBlockInfo processBlock(LevelReader level, BlockPos offset, BlockPos pos, StructureTemplate.StructureBlockInfo blockInfo, StructureTemplate.StructureBlockInfo relativeBlockInfo, StructurePlaceSettings settings) {
-        if (block.defaultBlockState().hasProperty(BlockStateProperties.FACING) && settings.getRandom(relativeBlockInfo.pos()).nextFloat() < chance)
-            return new StructureTemplate.StructureBlockInfo(relativeBlockInfo.pos(), block.defaultBlockState().setValue(BlockStateProperties.FACING, relativeBlockInfo.state().getValue(BlockStateProperties.FACING)), relativeBlockInfo.nbt());
-        return super.processBlock(level, offset, pos, blockInfo, relativeBlockInfo, settings);
+        if (relativeBlockInfo.state().is(old)) {
+            if (block.defaultBlockState().hasProperty(BlockStateProperties.FACING) && old.defaultBlockState().hasProperty(BlockStateProperties.FACING) &&
+                    settings.getRandom(relativeBlockInfo.pos()).nextFloat() < chance)
+                return new StructureTemplate.StructureBlockInfo(relativeBlockInfo.pos(), block.defaultBlockState().setValue(BlockStateProperties.FACING, relativeBlockInfo.state().getValue(BlockStateProperties.FACING)), relativeBlockInfo.nbt());
+        }return super.processBlock(level, offset, pos, blockInfo, relativeBlockInfo, settings);
     }
 
     @Override
