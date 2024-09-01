eb2d2cce94a2422a6fbaa03f725430f97b0dcf1d
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-01 12:45:09 -0400

    Add DirectionalBlockProcessor.java

---
+++ common/src/main/java/tech/jt_dev/compatprocessors/processor/DirectionalBlockProcessor.java
@@ -0,0 +1,49 @@
+package tech.jt_dev.compatprocessors.processor;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Replaces a block with another block based on chance
+ * Places the block in the same direction as the original block
+ * Useful for replacing blocks like furnaces with blast furnaces
+ */
+public class DirectionalBlockProcessor extends StructureProcessor {
+
+    public static final Codec<DirectionalBlockProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            BuiltInRegistries.BLOCK.byNameCodec().fieldOf("block").forGetter((block) -> block.block),
+            Codec.FLOAT.fieldOf("chance").forGetter((chance) -> chance.chance)
+    ).apply(instance, DirectionalBlockProcessor::new));
+
+    private final Block block;
+    private final float chance;
+
+    public DirectionalBlockProcessor(Block block, float chance) {
+        this.block = block;
+        this.chance = chance;
+    }
+
+
+    @Override
+    public @Nullable StructureTemplate.StructureBlockInfo processBlock(LevelReader level, BlockPos offset, BlockPos pos, StructureTemplate.StructureBlockInfo blockInfo, StructureTemplate.StructureBlockInfo relativeBlockInfo, StructurePlaceSettings settings) {
+        if (block.defaultBlockState().hasProperty(BlockStateProperties.FACING) && settings.getRandom(relativeBlockInfo.pos()).nextFloat() < chance)
+            return new StructureTemplate.StructureBlockInfo(relativeBlockInfo.pos(), block.defaultBlockState().setValue(BlockStateProperties.FACING, relativeBlockInfo.state().getValue(BlockStateProperties.FACING)), relativeBlockInfo.nbt());
+        return super.processBlock(level, offset, pos, blockInfo, relativeBlockInfo, settings);
+    }
+
+    @Override
+    protected @NotNull StructureProcessorType<?> getType() {
+        return null;
+    }
+}
