05c7e09ff670af95965fbb3b9d2dbd5b3479b9af
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-10 09:30:34 -0400

    Allow Placing a Specific BlockState for above block

--- common/src/main/java/tech/jt_dev/compatprocessors/processor/processors/PlaceOnTopProcessor.java
+++ common/src/main/java/tech/jt_dev/compatprocessors/processor/processors/PlaceOnTopProcessor.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -17,17 +18,23 @@ import tech.jt_dev.compatprocessors.processor.ProcessorRegister;
 public class PlaceOnTopProcessor extends StructureProcessor {
 	public static final Codec<PlaceOnTopProcessor> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			BuiltInRegistries.BLOCK.byNameCodec().fieldOf("below").forGetter((below) -> below.below),
-			BuiltInRegistries.BLOCK.byNameCodec().fieldOf("above").forGetter((block) -> block.above),
+			BlockState.CODEC.fieldOf("above").forGetter((block) -> block.above),
 			Codec.FLOAT.fieldOf("chance").forGetter((chance) -> chance.chance)
 	).apply(instance, PlaceOnTopProcessor::new));
 
 	private final Block below;
-	private final Block above;
+	private final BlockState above;
 	private final float chance;
+
+	public PlaceOnTopProcessor(Block below, BlockState above, float chance) {
+		this.below = below;
+		this.above = above;
+		this.chance = chance;
+	}
 
 	public PlaceOnTopProcessor(Block below, Block above, float chance) {
 		this.below = below;
-		this.above = above;
+		this.above = above.defaultBlockState();
 		this.chance = chance;
 	}
 
@@ -38,7 +45,7 @@ public class PlaceOnTopProcessor extends StructureProcessor {
 	@Override
 	public @Nullable StructureTemplate.StructureBlockInfo processBlock(@NotNull LevelReader level, @NotNull BlockPos offset, @NotNull BlockPos pos, StructureTemplate.@NotNull StructureBlockInfo blockInfo, StructureTemplate.@NotNull StructureBlockInfo relativeBlockInfo, @NotNull StructurePlaceSettings settings) {
 		if (relativeBlockInfo.state().is(below) && settings.getRandom(relativeBlockInfo.pos().above()).nextFloat() < chance)
-			return new StructureTemplate.StructureBlockInfo(relativeBlockInfo.pos().above(), above.defaultBlockState(), relativeBlockInfo.nbt());
+			return new StructureTemplate.StructureBlockInfo(relativeBlockInfo.pos().above(), above, relativeBlockInfo.nbt());
 		return super.processBlock(level, offset, pos, blockInfo, relativeBlockInfo, settings);
 	}
 
