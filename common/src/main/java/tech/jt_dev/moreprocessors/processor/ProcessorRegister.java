0f9c42f69622a65b417fa3875dde0ad86475e81d
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-04 22:52:54 -0400

    Remove Deprecated Stuff, Add some JavaDoc

--- common/src/main/java/tech/jt_dev/moreprocessors/processor/ProcessorRegister.java
+++ common/src/main/java/tech/jt_dev/moreprocessors/processor/ProcessorRegister.java
@@ -12,24 +12,24 @@ import tech.jt_dev.moreprocessors.processor.processors.CompatRuleProcessor;
 
 import java.util.function.Supplier;
 
+/**
+ * Class for registering all custom structure processors.
+ * @see StructureProcessorType
+ * @author Joseph T. McQuigg
+ */
 public class ProcessorRegister {
 
     public static final Supplier<StructureProcessorType<DirectionalBlockProcessor>> DIRECTIONAL_BLOCK_PROCESSOR = register("directional_block", () -> DirectionalBlockProcessor.CODEC);
     public static final Supplier<StructureProcessorType<PlaceOnTopProcessor>> PLACE_ON_TOP_PROCESSOR = register("place_on_top", () -> PlaceOnTopProcessor.CODEC);
     public static final Supplier<StructureProcessorType<PlaceBelowProcessor>> PLACE_BELOW_PROCESSOR = register("place_below", () -> PlaceBelowProcessor.CODEC);
     public static final Supplier<StructureProcessorType<RandomCropAgeProcessor>> RANDOM_CROP_AGE_PROCESSOR = register("random_crop_age", () -> RandomCropAgeProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<CompatReplaceProcessor>> COMPAT_REPLACE_PROCESSOR = register("compat_replace", () -> CompatReplaceProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<CompatReplaceSameStateProcessor>> COMPAT_REPLACE_SAME_STATE_PROCESSOR = register("compat_replace_same_state", () -> CompatReplaceSameStateProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<RandomCompatReplaceSameStateProcessor>> RANDOM_COMPAT_REPLACE_SAME_STATE_PROCESSOR = register("random_compat_replace_same_state", () -> RandomCompatReplaceSameStateProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<RandomReplaceSameStateProcessor>> RANDOM_REPLACE_SAME_STATE_PROCESSOR = register("random_replace_same_state", () -> RandomReplaceSameStateProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<RandomCompatProcessor>> RANDOM_COMPAT_PROCESSOR = register("random_compat", () -> RandomCompatProcessor.CODEC);
     public static final Supplier<StructureProcessorType<CompatRuleProcessor>> COMPAT_RULE = register("compat_rule", () -> CompatRuleProcessor.CODEC);
     public static final Supplier<StructureProcessorType<SameStateCompatRuleProcessor>> SAME_STATE_COMPAT_RULE = register("same_state_compat_rule", () -> SameStateCompatRuleProcessor.CODEC);
     public static final Supplier<StructureProcessorType<SameStateRuleProcessor>> SAME_STATE_RULE = register("same_state_rule", () -> SameStateRuleProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<ReplaceSameStateProcessor>> REPLACE_SAME_STATE_PROCESSOR = register("replace_same_state", () -> ReplaceSameStateProcessor.CODEC);
 
     /**
      * Utility method for registering custom structure processor types.
+     * @see PlatformHandler
      */
     private static <P extends StructureProcessor> Supplier<StructureProcessorType<P>> register(String id, @NotNull Supplier<Codec<P>> codec) {
         return PlatformHandler.PLATFORM_HANDLER.register(BuiltInRegistries.STRUCTURE_PROCESSOR, id, () -> codec::get);
