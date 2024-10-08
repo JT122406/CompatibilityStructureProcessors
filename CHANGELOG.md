e758891e52023adfdc9af0e75e332eb63ebf5458
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-10-08 16:10:04 -0400

    Remove Original Flowing Fluid Processor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # 1.1.0
+- Add Flowing Fluid Processor Rule and Rule Processor
 
 # 1.0.0
 - Initial release
--- common/src/main/java/tech/jt_dev/moreprocessors/processor/ProcessorRegister.java
+++ common/src/main/java/tech/jt_dev/moreprocessors/processor/ProcessorRegister.java
@@ -26,7 +26,6 @@ public class ProcessorRegister {
     public static final Supplier<StructureProcessorType<CompatRuleProcessor>> COMPAT_RULE = register("compat_rule", () -> CompatRuleProcessor.CODEC);
     public static final Supplier<StructureProcessorType<SameStateCompatRuleProcessor>> SAME_STATE_COMPAT_RULE = register("same_state_compat_rule", () -> SameStateCompatRuleProcessor.CODEC);
     public static final Supplier<StructureProcessorType<SameStateRuleProcessor>> SAME_STATE_RULE = register("same_state_rule", () -> SameStateRuleProcessor.CODEC);
-    public static final Supplier<StructureProcessorType<FlowingFluidProcessor>> FLOWING_FLUID_PROCESSOR = register("flowing_fluid", () -> FlowingFluidProcessor.CODEC);
     public static final Supplier<StructureProcessorType<FlowingFluidRuleProcessor>> FLOWING_FLUID_RULE = register("flowing_fluid_rule", () -> FlowingFluidRuleProcessor.CODEC);
 
     /**
